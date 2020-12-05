d68676e4e66e9209b3dd42f858a5a2cddb130971
SONPYEONGHWA <ssrsph@naver.com>
2020-12-05 13:44:05 +0900

    feat: 스크롤 렉 수정, RetrofitClient 생성

--- app/src/main/java/com/example/a27thandroidjoint/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/a27thandroidjoint/MainViewPagerAdapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import com.example.a27thandroidjoint.home.HomeFragment
-import com.example.a27thandroidjoint.mainfeed.PostingFragment
+import com.example.a27thandroidjoint.mainfeed.fragment.PostingFragment
 import com.example.a27thandroidjoint.mypage.MyPageFragment
 import java.lang.IllegalStateException
 
